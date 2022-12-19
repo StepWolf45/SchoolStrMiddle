f892ba9769c69fce1602ebf0f65709d0db336e8d
Stepan Andreichev <85014298+StepWolf45@users.noreply.github.com>
2022-12-19 15:09:10 +0300

    Add files via upload

--- middle_str.h
+++ middle_str.h
@@ -9,7 +9,7 @@ long long itc_len(string str);
 bool itc_isDigit(unsigned char c);
 unsigned char itc_toUpper(unsigned char c);
 unsigned char itc_changeCase(unsigned char c);
-bool itc_сompare(string s1, string s2);
+bool itc_compare(string s1, string s2);
 int itc_countWords(string str);
 string itc_maxCharWord(string str);
 char itc_sameChar(string str);
